7c9b1d1a4b52cb1a60f8538a8b4b77c9ac722ede
= <=>
2017-10-02 18:10:49 -0300

    visitor add recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,19 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new
+    @recipe.title = params[:recipe][:title]
+    @recipe.recipe_type = params[:recipe][:recipe_type]
+    @recipe.cuisine = params[:recipe][:cuisine]
+    @recipe.difficulty = params[:recipe][:difficulty]
+    @recipe.cook_time = params[:recipe][:cook_time]
+    @recipe.ingredients = params[:recipe][:ingredients]
+    @recipe.method = params[:recipe][:method]
+    @recipe.save
+    redirect_to recipe_url(@recipe.id)
+  end
 end
