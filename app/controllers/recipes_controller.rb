825d54bfe17501d9392043c4c0b5fc6a6a599bbd
= <=>
2017-10-02 18:13:10 -0300

    security params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,14 +6,16 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   def create
-    @recipe = Recipe.new
-    @recipe.title = params[:recipe][:title]
-    @recipe.recipe_type = params[:recipe][:recipe_type]
-    @recipe.cuisine = params[:recipe][:cuisine]
-    @recipe.difficulty = params[:recipe][:difficulty]
-    @recipe.cook_time = params[:recipe][:cook_time]
-    @recipe.ingredients = params[:recipe][:ingredients]
-    @recipe.method = params[:recipe][:method]
+
+    recipe_params = params.require(:recipe).permit(:title,
+                                    :recipe_type,
+                                    :cuisine,
+                                    :difficulty,
+                                    :cook_time,
+                                    :ingredients,
+                                    :method)
+                                    
+    @recipe = Recipe.new(recipe_params)
     @recipe.save
     redirect_to recipe_url(@recipe.id)
   end
