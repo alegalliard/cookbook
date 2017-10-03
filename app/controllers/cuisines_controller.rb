0734f1d6b9e3ee7b51f47b62b6389601f3138867
= <=>
2017-10-03 18:10:36 -0300

    new cuisine

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,4 +2,22 @@ class CuisinesController < ApplicationController
   def show
     @cuisine = Cuisine.find(params[:id])
   end
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(cuisine_params)
+    if @cuisine.save
+      redirect_to cuisine_url(@cuisine.id)
+    else
+      flash[:error] = 'Você deve informar o nome da cozinha'
+      redirect_to new_cuisine_path
+    end
+  end
+  private
+
+  def cuisine_params
+    params.require(:cuisine).permit(:name)
+  end
 end
